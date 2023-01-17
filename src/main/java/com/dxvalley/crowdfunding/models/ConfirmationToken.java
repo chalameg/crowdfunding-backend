d7a8c22f0251b9055beeb8a9320947e7bf1af681
Abdi Tiruneh <abdi.tirunehh@gmail.com>
2023-01-17 14:01:59 +0300

    add email verification token Model,Service and Repository

---
+++ src/main/java/com/dxvalley/crowdfunding/models/ConfirmationToken.java
@@ -0,0 +1,42 @@
+package com.dxvalley.crowdfunding.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class ConfirmationToken {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String token;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
+
+    private LocalDateTime confirmedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "app_user_id")
+    private Users appUser;
+
+    public ConfirmationToken(String token,
+                             LocalDateTime createdAt,
+                             LocalDateTime expiresAt,
+                             Users users) {
+        this.token = token;
+        this.createdAt = createdAt;
+        this.expiresAt = expiresAt;
+        this.appUser = users;
+    }
+}
