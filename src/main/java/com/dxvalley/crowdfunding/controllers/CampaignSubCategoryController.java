b8ec3207185600bd0f46ce26fe8614f645e18468
Abdi Tiruneh <abdi.tirunehh@gmail.com>
2023-01-23 10:11:40 +0300

    Add the ability to search for campaign sub-categories by campaign category.

--- src/main/java/com/dxvalley/crowdfunding/controllers/CampaignSubCategoryController.java
+++ src/main/java/com/dxvalley/crowdfunding/controllers/CampaignSubCategoryController.java
@@ -2,6 +2,8 @@ package com.dxvalley.crowdfunding.controllers;
 
 import java.util.List;
 
+import com.dxvalley.crowdfunding.repositories.CampaignCategoryRepository;
+import com.dxvalley.crowdfunding.repositories.CampaignSubCategoryRepository;
 import com.dxvalley.crowdfunding.services.CampaignCategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -23,7 +25,8 @@ import com.dxvalley.crowdfunding.services.CampaignSubCategoryService;
 @RequestMapping("/api/campaignSubCategory")
 public class CampaignSubCategoryController {
   private final CampaignSubCategoryService campaignSubCategoryService;
-  private final CampaignCategoryService campaignCategoryService;
+  private final CampaignCategoryRepository campaignCategoryRepository;
+  private final CampaignSubCategoryRepository campaignSubCategoryRepository;
 
   @GetMapping("/getSubCategories")
   List<CampaignSubCategory> getCampaignSubcategories() {
@@ -39,10 +42,29 @@ public class CampaignSubCategoryController {
     }
     return new ResponseEntity<>(result, HttpStatus.OK);
   }
-
+  @GetMapping("/getByCategoryId/{campaignCategoryId}")
+  ResponseEntity<?> getCampaignSubCategoryByCategory(@PathVariable Long campaignCategoryId) {
+    var result =  campaignSubCategoryRepository.findByCampaignCategoryId(campaignCategoryId);
+    System.out.println(result.size());
+    if (result.size() == 0){
+      return new ResponseEntity<>("There is no subcategory with this Category Id.", HttpStatus.NOT_FOUND);
+    }
+    return new ResponseEntity<>(result, HttpStatus.OK);
+  }
   @PostMapping("/add")
   ResponseEntity<?> addCampaignSubCategory(@RequestBody CampaignSubCategory tempCampaignSubCategories) {
     //TODO: check existence of category before adding subcategory
+
+    if(tempCampaignSubCategories.getCampaignCategory() == null){
+      return new ResponseEntity<>("Campaign category is required field", HttpStatus.BAD_REQUEST);
+    }
+    var result = campaignCategoryRepository.findCampaignCategoryByCampaignCategoryId(
+            tempCampaignSubCategories.getCampaignCategory().getCampaignCategoryId());
+
+    if (result == null){
+      return new ResponseEntity<>("there is no category with this id", HttpStatus.BAD_REQUEST);
+    }
+
     var campaignSubCategories =  campaignSubCategoryService.addCampaignSubCategory(tempCampaignSubCategories);
       return new ResponseEntity<>(campaignSubCategories, HttpStatus.OK);
   }
@@ -67,10 +89,22 @@ public class CampaignSubCategoryController {
                     tempSubCampaignCategory.getDescription() :
                     campaignSubCategory.getDescription());
     //TODO: check existence of category before updating subcategory
-    campaignSubCategory.setCategory(
-            tempSubCampaignCategory.getCategory() != null ?
-                    tempSubCampaignCategory.getCategory() :
-                    campaignSubCategory.getCategory());
+
+    if(tempSubCampaignCategory.getCampaignCategory() != null){
+      System.out.println("hello");
+      var result = campaignCategoryRepository.findById(
+              tempSubCampaignCategory.getCampaignCategory().getCampaignCategoryId());
+
+      System.out.println(result);
+      if (result !=null){
+        campaignSubCategory.setCampaignCategory(
+                        campaignSubCategory.getCampaignCategory());
+      }
+      return new ResponseEntity<>("there is no category with this id", HttpStatus.OK);
+    }
+
+
+    System.out.println(tempSubCampaignCategory.getCampaignCategory().getCampaignCategoryId());
 
     var result =  campaignSubCategoryService.editCampaignSubCategory(campaignSubCategory);
     return new ResponseEntity<>(result, HttpStatus.OK);
--- src/main/java/com/dxvalley/crowdfunding/models/CampaignCategory.java
+++ src/main/java/com/dxvalley/crowdfunding/models/CampaignCategory.java
@@ -1,12 +1,12 @@
 package com.dxvalley.crowdfunding.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
