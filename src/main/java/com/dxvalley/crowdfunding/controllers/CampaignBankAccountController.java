a5e5fe295661c7356d56ecf8c1c282a26328f1c5
ABDI TIRUNEH <abdi.tirunehh@gmail.com>
2023-02-25 11:46:50 +0300

    update Campaign Bank Account

--- src/main/java/com/dxvalley/crowdfunding/controllers/CampaignBankAccountController.java
+++ src/main/java/com/dxvalley/crowdfunding/controllers/CampaignBankAccountController.java
@@ -1,9 +1,6 @@
 package com.dxvalley.crowdfunding.controllers;
 
-import com.dxvalley.crowdfunding.models.CampaignBankAccount;
-import com.dxvalley.crowdfunding.repositories.CampaignBankAccountRepository;
 import com.dxvalley.crowdfunding.services.CampaignBankAccountService;
-import com.dxvalley.crowdfunding.services.CampaignService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
@@ -12,45 +9,25 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/campaignBankAccount")
 public class CampaignBankAccountController {
     @Autowired
-    private CampaignBankAccountRepository campaignBankAccountRepository;
-    @Autowired
-    private CampaignService campaignService;
-    @Autowired
     CampaignBankAccountService campaignBankAccountService;
-    
-    @GetMapping("/getCampaignBankAccountByCampaign/{campaignId}")
-    CampaignBankAccount getCampaignBankAccountByCampaign(@PathVariable Long campaignId) {
-        campaignBankAccountService.getCampaignBankAccountByCampaignId(campaignId);
 
-        return campaignBankAccountRepository.findCampaignBankAccountByCampaignId(campaignId);
+    @GetMapping("/getCampaignBankAccountByCampaign/{campaignId}")
+    ResponseEntity<?> getCampaignBankAccountByCampaign(@PathVariable Long campaignId) {
+        var campaignBankAccount = campaignBankAccountService.getCampaignBankAccountByCampaignId(campaignId);
+        return new ResponseEntity<>(campaignBankAccount, HttpStatus.OK);
     }
 
-    @PostMapping("/add/{campaignId}")
+    @PostMapping("/add/{campaignId}/{bankAccount}")
     public ResponseEntity<?> addCampaignBankAccount(
             @PathVariable Long campaignId,
-            @RequestParam String bankAccount) {
-
-        campaignBankAccountService.addBankAccount(campaignId, bankAccount);
-
-        CampaignBankAccount campaignBankAccount = new CampaignBankAccount();
-
-        // var result = campaignBankAccountRepository.findCampaignBankAccountByBankAccount(bankAccount);
-        // if (result != null){
-        //     return new  ResponseEntity<>( bankAccount + " already registered", HttpStatus.BAD_REQUEST);
-        // }
-        var camp =  campaignBankAccountRepository.findCampaignBankAccountByCampaignId(campaignId);
-        if(camp != null){
-            return new  ResponseEntity<>( "This campaign already have bank account. try updating", HttpStatus.BAD_REQUEST);
-        }
-        var campaign = campaignService.getCampaignById(campaignId);
-
-        campaignBankAccount.setBankAccount(bankAccount);
-        campaignBankAccount.setCampaign(campaign);
-        return new ResponseEntity<>(campaignBankAccountRepository.save(campaignBankAccount),HttpStatus.OK);
+            @PathVariable String bankAccount) {
+        var campaignBankAccount = campaignBankAccountService.addBankAccount(campaignId, bankAccount);
+        return new ResponseEntity<>(campaignBankAccount, HttpStatus.OK);
     }
+
     @GetMapping("/checkBankAccountExistence/{bankAccount}")
     ResponseEntity<?> checkBankAccountExistence(@PathVariable String bankAccount) throws Exception {
-       var response =  campaignBankAccountService.checkBankAccountExistence(bankAccount);
-        return new ResponseEntity<>(response,HttpStatus.OK);
+        var response = campaignBankAccountService.checkBankAccountExistence(bankAccount);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
