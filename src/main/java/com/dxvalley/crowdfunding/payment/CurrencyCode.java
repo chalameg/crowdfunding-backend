54dd8ba262a4e1e5a5831ae0c34784bd246ac249
ABDI TIRUNEH <abdi.tirunehh@gmail.com>
2023-05-10 14:38:22 +0300

    add CurrencyCode

---
+++ src/main/java/com/dxvalley/crowdfunding/payment/CurrencyCode.java
@@ -0,0 +1,15 @@
+package com.dxvalley.crowdfunding.payment;
+
+import java.util.Arrays;
+public enum CurrencyCode {
+    ETB,
+    EUR,
+    GBP,
+    USD,
+    AED;
+    public static CurrencyCode lookup(String currencyCode) {
+        return Arrays.stream(CurrencyCode.values())
+                .filter(e -> e.name().equalsIgnoreCase(currencyCode)).findAny()
+                .orElseThrow(()-> new IllegalArgumentException("Invalid Currency Code."));
+    }
+}
