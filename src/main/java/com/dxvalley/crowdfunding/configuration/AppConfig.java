784095c8f54c429a49b14cbaf4e91349370346da
ABDI TIRUNEH <abdi.tirunehh@gmail.com>
2023-05-10 14:49:51 +0300

    update Appconfig

--- src/main/java/com/dxvalley/crowdfunding/configuration/AppConfig.java
+++ src/main/java/com/dxvalley/crowdfunding/configuration/AppConfig.java
@@ -3,8 +3,10 @@ package com.dxvalley.crowdfunding.configuration;
 import com.cloudinary.Cloudinary;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -13,21 +15,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@EnableScheduling
 public class AppConfig {
-    private final String cloudName = "do394twgw";
-    private final String apiKey = "563676698242191";
-    private final String apiSecret = "fYRKyRcysOZmtWP06cgXRu_Fpq0";
 
-//
-//
-//    public AppConfig(@Value("${CLOUDINARY_CLOUD_NAME}") String cloudName,
-//                     @Value("${CLOUDINARY_API_KEY}") String apiKey,
-//                     @Value("${CLOUDINARY_API_SECRET}") String apiSecret) {
-//        this.cloudName = cloudName;
-//        this.apiKey = apiKey;
-//        this.apiSecret = apiSecret;
-//    }
+    private final String cloudName;
+    private final String apiKey;
+    private final String apiSecret;
 
+    public AppConfig(@Value("${CLOUDINARY.CLOUD_NAME}") String cloudName,
+                            @Value("${CLOUDINARY.API_KEY}") String apiKey,
+                            @Value("${CLOUDINARY.API_SECRET}") String apiSecret) {
+        this.cloudName = cloudName;
+        this.apiKey = apiKey;
+        this.apiSecret = apiSecret;
+    }
+
+    @Bean
+    public Cloudinary cloudinaryConfig() {
+        try {
+            Map<String, String> config = new HashMap<>();
+            config.put("cloud_name", cloudName);
+            config.put("api_key", apiKey);
+            config.put("api_secret", apiSecret);
+            return new Cloudinary(config);
+        } catch (Exception ex) {
+            throw new IllegalStateException("Error creating Cloudinary bean.", ex);
+        }
+    }
     @Bean
     public DateTimeFormatter dateTimeFormatter() {
         return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
@@ -45,16 +59,4 @@ public class AppConfig {
                 .create();
     }
 
-    @Bean
-    public Cloudinary cloudinaryConfig() {
-        try {
-            Map<String, String> config = new HashMap<>();
-            config.put("cloud_name", cloudName);
-            config.put("api_key", apiKey);
-            config.put("api_secret", apiSecret);
-            return new Cloudinary(config);
-        } catch (Exception ex) {
-            throw new IllegalStateException("Error creating Cloudinary bean.", ex);
-        }
-    }
 }
