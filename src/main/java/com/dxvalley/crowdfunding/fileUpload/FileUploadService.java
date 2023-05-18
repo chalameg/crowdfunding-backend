e7c8bc5907b5944640b0111d731a2da6e02400be
ABDI TIRUNEH <abdi.tirunehh@gmail.com>
2023-05-18 17:48:04 +0300

    Refactor the file upload package

--- src/main/java/com/dxvalley/crowdfunding/fileUpload/FileUploadService.java
+++ src/main/java/com/dxvalley/crowdfunding/fileUpload/FileUploadService.java
@@ -2,6 +2,9 @@ package com.dxvalley.crowdfunding.fileUpload;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 public interface FileUploadService {
      String uploadFile(MultipartFile multipartFile);
+     List<String> uploadFiles(List<MultipartFile> files);
 }
