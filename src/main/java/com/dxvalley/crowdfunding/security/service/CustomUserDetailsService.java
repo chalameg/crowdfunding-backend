5baeb661ac20b633d39d8492080eaaf6eb5b396e
ABDI TIRUNEH <abdi.tirunehh@gmail.com>
2023-05-24 12:00:07 +0300

    refactor CustomUserDetailsService.java

--- src/main/java/com/dxvalley/crowdfunding/security/service/CustomUserDetailsService.java
+++ src/main/java/com/dxvalley/crowdfunding/security/service/CustomUserDetailsService.java
@@ -1,7 +1,7 @@
 package com.dxvalley.crowdfunding.security.service;
 
-import com.dxvalley.crowdfunding.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.dxvalley.crowdfunding.user.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,13 +12,13 @@ import java.util.Collection;
 import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        var user = userRepository.findUsersByUsernameAndIsEnabled(username, true).orElseThrow(
+        var user = userRepository.findByUsername(username).orElseThrow(
                 () -> new UsernameNotFoundException("There is no user with this username")
         );
 
@@ -31,11 +31,3 @@ public class CustomUserDetailsService implements UserDetailsService {
         );
     }
 }
-
-//        if (user.isPresent()) {
-//            Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
-//            user.getRoles().forEach(role -> authorities.add(new SimpleGrantedAuthority(role.getRoleName())));
-//            return new org.springframework.security.core.userdetails.User(
-//                    user.getUsername(), user.getPassword(), authorities
-//            );
-//        }
