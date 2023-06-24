651800244775b00248015b1195967d4815bda51d
ABDI TIRUNEH <abdi.tirunehh@gmail.com>
2023-06-24 10:06:27 +0300

    add file to campaignMedia

--- src/main/java/com/dxvalley/crowdfunding/campaign/campaign/campaignMedia/image/CampaignImageService.java
+++ src/main/java/com/dxvalley/crowdfunding/campaign/campaign/campaignMedia/image/CampaignImageService.java
@@ -1,10 +1,15 @@
 package com.dxvalley.crowdfunding.campaign.campaign.campaignMedia.image;
 
+import com.dxvalley.crowdfunding.campaign.campaign.Campaign;
+import com.dxvalley.crowdfunding.campaign.campaign.campaignUtils.CampaignUtils;
 import com.dxvalley.crowdfunding.fileUploadManager.FileUploadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Service
 @Transactional
@@ -12,20 +17,46 @@ import org.springframework.web.multipart.MultipartFile;
 public class CampaignImageService {
     private final CampaignImageRepository campaignImageRepository;
     private final FileUploadService fileUploadService;
+    private final CampaignUtils campaignUtils;
+    private final DateTimeFormatter dateTimeFormatter;
 
-    public CampaignImage saveCampaignImage(MultipartFile multipartFile) {
-        String imageUrl = fileUploadService.uploadFile(multipartFile);
-        
-        CampaignImage campaignImage = CampaignImage.builder()
-                .imageUrl(imageUrl)
-                .isPrimary(true)
-                .build();
+    public CampaignImage addImage(Long campaignId, MultipartFile multipartFile) {
+        Campaign campaign = this.campaignUtils.utilGetCampaignById(campaignId);
+        boolean imageExist = this.isImageExist(campaign);
+        CampaignImage campaignImage = this.saveCampaignImage(multipartFile, imageExist);
+        this.updateCampaignWithImage(campaign, campaignImage);
+        return campaignImage;
+    }
 
-        return campaignImageRepository.save(campaignImage);
+    public CampaignImage addImage(Campaign campaign, MultipartFile multipartFile) {
+        boolean imageExist = this.isImageExist(campaign);
+        CampaignImage campaignImage = this.saveCampaignImage(multipartFile, imageExist);
+        this.updateCampaignWithImage(campaign, campaignImage);
+        return campaignImage;
+    }
+
+    private void updateCampaignWithImage(Campaign campaign, CampaignImage campaignImage) {
+        campaign.addImage(campaignImage);
+        campaign.setEditedAt(LocalDateTime.now().format(this.dateTimeFormatter));
+        this.campaignUtils.saveCampaign(campaign);
+    }
+
+    private boolean isImageExist(Campaign campaign) {
+        return !campaign.getImages().isEmpty();
+    }
+
+    public CampaignImage saveCampaignImage(MultipartFile multipartFile, boolean imageExist) {
+        String imageUrl = this.fileUploadService.uploadFile(multipartFile);
+        CampaignImage campaignImage = this.buildCampaignImage(imageUrl, imageExist);
+        return (CampaignImage)this.campaignImageRepository.save(campaignImage);
+    }
+
+    private CampaignImage buildCampaignImage(String imageUrl, boolean imageExist) {
+        return CampaignImage.builder().imageUrl(imageUrl).isPrimary(!imageExist).build();
     }
 
     public void deleteCampaignImage(Long imageId) {
-        campaignImageRepository.deleteById(imageId);
+        this.campaignImageRepository.deleteById(imageId);
     }
 
 }
--- src/main/java/com/dxvalley/crowdfunding/campaign/campaign/campaignMedia/video/CampaignVideoService.java
+++ src/main/java/com/dxvalley/crowdfunding/campaign/campaign/campaignMedia/video/CampaignVideoService.java
@@ -1,31 +1,48 @@
 package com.dxvalley.crowdfunding.campaign.campaign.campaignMedia.video;
 
+import com.dxvalley.crowdfunding.campaign.campaign.Campaign;
+import com.dxvalley.crowdfunding.campaign.campaign.campaignUtils.CampaignUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class CampaignVideoService {
     private final CampaignVideoRepository campaignVideoRepository;
+    private final CampaignUtils campaignUtils;
+    private final DateTimeFormatter dateTimeFormatter;
+
+    public CampaignVideo addVideo(Long campaignId, String campaignVideoUrl) {
+        Campaign campaign = this.campaignUtils.utilGetCampaignById(campaignId);
+        CampaignVideo campaignVideo = this.saveCampaignVideo(campaignVideoUrl);
+        this.updateCampaignWithVideo(campaign, campaignVideo);
+        return campaignVideo;
+    }
+
+    public CampaignVideo addVideo(Campaign campaign, String campaignVideoUrl) {
+        CampaignVideo campaignVideo = this.saveCampaignVideo(campaignVideoUrl);
+        this.updateCampaignWithVideo(campaign, campaignVideo);
+        return campaignVideo;
+    }
 
     public CampaignVideo saveCampaignVideo(String videoUrl) {
-        CampaignVideo campaignVideo = CampaignVideo.builder()
-                .videoUrl(videoUrl)
-                .isPrimary(true)
-                .build();
-        return campaignVideoRepository.save(campaignVideo);
+        CampaignVideo campaignVideo = CampaignVideo.builder().videoUrl(videoUrl).isPrimary(true).build();
+        return (CampaignVideo) this.campaignVideoRepository.save(campaignVideo);
+    }
+
+    private void updateCampaignWithVideo(Campaign campaign, CampaignVideo campaignVideo) {
+        campaign.setVideo(campaignVideo);
+        campaign.setEditedAt(LocalDateTime.now().format(this.dateTimeFormatter));
+        this.campaignUtils.saveCampaign(campaign);
     }
 
     public void deleteCampaignVideo(Long videoId) {
-        campaignVideoRepository.deleteById(videoId);
-    }
-
-    public Optional<CampaignVideo> getCampaignVideoById(Long videoId) {
-        return campaignVideoRepository.findById(videoId);
+        this.campaignVideoRepository.deleteById(videoId);
     }
 
 }
